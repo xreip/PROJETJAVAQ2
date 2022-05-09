ac0698e1534ddd6a386843d87e2631ce0f751b89
xreip <pierre.mouffe@gmail.com>
2022-05-09 18:21:44 +0200

    panneauUninstall

---
+++ src/examen/PanneauUninstall.java
@@ -0,0 +1,31 @@
+package examen;
+import javax.swing.*;
+
+import accessDB.AccessBDGen;
+import accessDB.TableModelGen;
+import accessDB.ConnectionBD;
+
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import java.sql.*;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+
+public class PanneauUninstall extends JPanel {
+   private JLabel osChoice;
+   private JComboBox<Object[]> osChoiceComboBox;
+
+   public PanneauUninstall(){
+
+   }
+   
+}
